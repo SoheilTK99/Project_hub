339d2f903eb10145d60fa29d6b5ac045eb5aaaff
Soheil <soheil.ce.99@gmail.com>
2025-10-29 18:19:59 +0330

    Service_detail/2

--- service/urls.py
+++ service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .import views
 
+app_name = 'service'
 
 urlpatterns = [
-    path('', views.service_list, name='service'),
-    path('<int:pk>/', views.service_detail, name='service_detail'),
+    path('', views.service_list, name='list'),
+    path('<int:pk>/', views.service_detail, name='detail'),
     
 ]
 
--- service/views.py
+++ service/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Service
 
-
-
 def service_list(request):
     services = Service.objects.all()
     return render(request, "service/service.html", {"services": services})
@@ -11,15 +9,14 @@ def service_list(request):
 def service_detail(request, pk):
     service = get_object_or_404(Service, pk=pk)
     
-    # لیست تمام تصاویر
-    images = [
+    # فقط تصاویر غیرخالی
+    image_fields = [
         service.image_1, service.image_2, service.image_3, service.image_4,
         service.image_5, service.image_6, service.image_7, service.image_8
     ]
-    # فقط تصاویری که وجود دارن
-    images = [img for img in images if img]
-
+    images = [img for img in image_fields if img]
+    
     return render(request, "service/service_detail.html", {
         "service": service,
-        "images": images  # لیست تصاویر
+        "images": images
     })
