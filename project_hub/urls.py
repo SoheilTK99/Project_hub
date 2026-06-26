5a8c34b25cdd7f08e0e13d1200a6aaa5937fc0a2
Soheil <soheil.ce.99@gmail.com>
2026-06-26 22:20:36 +0330

    update1

--- project_hub/urls.py
+++ project_hub/urls.py
@@ -20,6 +20,8 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 from django.shortcuts import redirect
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('', lambda request: redirect('/home/')),
@@ -30,6 +32,6 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('webdesign/', TemplateView.as_view(template_name='service/webdesign.html'), name='webdesign'),
     path('seo/', TemplateView.as_view(template_name='service/seo.html'), name='seo'),     
-] 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
